00b20d432bb36601236cd4f2b98518b0e8627f4b
synpixel <syntaxwastaken2@gmail.com>
2023-07-08 17:50:49 +0200

    fix LogLevel export

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,10 +1,13 @@
-export interface LogLevel {
+export declare const LogLevel: {
     None: 0,
     Warn: 1,
     Info: 2,
     Debug: 3
 }
 
+export type LogLevel = 0 | 1 | 2 | 3
+
 export { default as Store } from "./Store"
 export { default as Session } from "./Session"
+
 export declare function setLogLevel(logLevel: LogLevel): void
